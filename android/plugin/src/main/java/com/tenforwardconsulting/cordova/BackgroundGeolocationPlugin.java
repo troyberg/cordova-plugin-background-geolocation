f12258ebb8d7f5f63477d1af4a29c93d9cdc4c68
troyberg <troy.cflweb@gmail.com>
2017-07-31 14:40:10 -0400

    Forced test null pointer crash

--- android/plugin/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
+++ android/plugin/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
@@ -674,6 +674,7 @@ public class BackgroundGeolocationPlugin extends CordovaPlugin {
         for (int r : grantResults) {
             if (r == PackageManager.PERMISSION_DENIED) {
                 log.info("Permission Denied!");
+                actionStartCallbackContext = null;
                 actionStartCallbackContext.error(JSONErrorFactory.getJSONError(PERMISSION_DENIED_ERROR_CODE, "Permission denied by user"));
                 actionStartCallbackContext = null;
                 return;
@@ -682,6 +683,7 @@ public class BackgroundGeolocationPlugin extends CordovaPlugin {
         switch (requestCode) {
             case START_REQ_CODE:
                 startAndBindBackgroundService();
+                actionStartCallbackContext = null;
                 actionStartCallbackContext.success();
                 actionStartCallbackContext = null;
                 break;
